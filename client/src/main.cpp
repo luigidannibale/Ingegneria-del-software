9fd21ba6f27ca6b32cf647f39ee456786e803bcf
Daniele Venturini <venturini.1991255@studenti.uniroma1.it>
2023-12-27 16:24:36 +0100

    Added wxWidgets Co-authored-by: luigidannibale <luigidannibale@users.noreply.github.com>

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,7 +1,21 @@
 #include "main.h"
+#include <wx/wx.h>
 
-int main() {
-    // istanzia View
-    // istanzia Model(View)
-    // istanzia Controller(View, Model)
-}
+// int main() {
+//     // MyApp a = new MyApp();    
+//     // istanzia View
+//     // istanzia Model(View)
+//     // istanzia Controller(View, Model)
+// }
+
+
+class MyApp : public wxApp {
+public:
+    virtual bool OnInit() {
+        wxFrame *frame = new wxFrame(nullptr, wxID_ANY, "Hello wxWidgets");
+        frame->Show(true);
+        return true;
+    }
+};
+
+wxIMPLEMENT_APP(MyApp);
