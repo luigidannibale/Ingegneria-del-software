0e698de3f2db56d3dc93ae204d8ec8671b12249e
Dalphan <danyventurini25@gmail.com>
2024-06-16 22:36:11 +0200

    adjusted some graphics

--- client/src/view/GameplayFrame.cpp
+++ client/src/view/GameplayFrame.cpp
@@ -13,38 +13,42 @@ GameplayFrame::GameplayFrame(bool isWhite, GameOptions* options): wxFrame(NULL, 
     Show();
     SetBackgroundColour(wxColour(118,150,86));
 
-    wxImage boardImg = img::GetImageAndScale(IMGPATH + "chessboard-blue.png",scal);
-    chessboard = new ChessboardView(new wxStaticBitmap(this, wxID_ANY, wxBitmap(boardImg)),"icons/", chessX, chessY, isWhite,scal);
+    wxSizer *mainSizer = new wxBoxSizer(wxHORIZONTAL);
+    wxPanel *leftPanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+    wxPanel *rightPanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
 
-    opponentText = new wxStaticText(this, wxID_ANY,wxString("Opponent name"));
-    opponentText->Move(100*scal,20*scal);
-    userText = new wxStaticText(this, wxID_ANY,wxString("Io"));
-    userText->Move(100*scal,750*scal);
+    wxImage boardImg = img::GetImageAndScale(IMGPATH + "chessboard-blue.png",scal);
+    chessboard = new ChessboardView(new wxStaticBitmap(leftPanel, wxID_ANY, wxBitmap(boardImg)),"icons/", chessX, chessY, isWhite,scal);
+
+    opponentText = new wxStaticText(rightPanel, wxID_ANY,wxString("Opponent name"));
+    opponentText->Move(100*scal,60*scal);
+    userText = new wxStaticText(rightPanel, wxID_ANY,wxString("Io"));
+    userText->Move(100*scal,460*scal);
 
 
     const char* labelY[] = {"87654321","12345678"};
     const char* labelX[] =  {"ABCDEFGH","HGFEDCBA"};
 
     wxFont font(wxFontInfo(14).Family(wxFONTFAMILY_MODERN));
-    wxStaticText* prova = new wxStaticText(this, wxID_ANY, wxString(""));
+    wxStaticText* prova = new wxStaticText(leftPanel, wxID_ANY, wxString(""));
     wxSize fontSize = prova->GetFont().GetPixelSize();
 
     int index = isWhite?0:1;
     for (int i = 0; i < 8; i ++) {
-        wxStaticText* label = new wxStaticText(this, wxID_ANY, wxString(labelY[index][i]));
+        wxStaticText* label = new wxStaticText(leftPanel, wxID_ANY, wxString(labelY[index][i]));
         // label->SetFont(font);
         label->SetForegroundColour(wxColour(0, 0, 0)); // Black color
         label->Move(chessboard->GetBoard()->GetSize().GetWidth() + chessX+5, (chessY + i * GetChessboard()->GetCellDimension() + (GetChessboard()->GetCellDimension()/2 + fontSize.GetHeight()) / 2));
     }
     for (int i = 0; i < 8; i ++) {
-        wxStaticText* label = new wxStaticText(this, wxID_ANY, wxString(labelX[index][i]));
+        wxStaticText* label = new wxStaticText(leftPanel, wxID_ANY, wxString(labelX[index][i]));
         // label-SetFont(font);
         label->SetForegroundColour(wxColour(0, 0, 0)); // Black color
         label->Move(chessX+ i*GetChessboard()->GetCellDimension() + GetChessboard()->GetCellDimension()/2 , chessY-fontSize.GetHeight()-5);
     }
 
 
-    movesPlayedList = new wxListView(this, wxID_ANY, wxPoint(800*scal, 200*scal), wxSize(300*scal, 200*scal), wxLC_REPORT);
+    movesPlayedList = new wxListView(rightPanel, wxID_ANY, wxPoint(80*scal, 200*scal), wxSize(300*scal, 200*scal), wxLC_REPORT);
     movesPlayedList->InsertColumn(0, "Num.", wxLIST_FORMAT_LEFT, 50*scal);
     movesPlayedList->InsertColumn(1, "White Moves", wxLIST_FORMAT_LEFT, 100*scal);
     movesPlayedList->InsertColumn(2, "Black Moves", wxLIST_FORMAT_LEFT, 150*scal);
@@ -52,21 +56,29 @@ GameplayFrame::GameplayFrame(bool isWhite, GameOptions* options): wxFrame(NULL, 
     this->whiteSeconds = options->GetGameDurationInSeconds();
     this->blackSeconds = options->GetGameDurationInSeconds();
     this->increment = options->GetGameIncrement();
-    whiteStatText = new wxStaticText(this, wxID_ANY,wxString("White timer"));
-    whiteStatText->Move(1000*scal,60*scal);
-    blackStatText = new wxStaticText(this, wxID_ANY,wxString("Black timer"));
-    blackStatText->Move(1000*scal,460*scal);
+    // whiteStatText = new wxStaticText(rightPanel, wxID_ANY,wxString("White timer"));
+    // whiteStatText->Move(100*scal,60*scal);
+    // blackStatText = new wxStaticText(rightPanel, wxID_ANY,wxString("Black timer"));
+    // blackStatText->Move(100*scal,460*scal);
 
     std::string time = secondsToString(options->GetGameDurationInSeconds());
-    whiteTimerText = new wxStaticText(this, wxID_ANY, wxString(time));
-    blackTimerText = new wxStaticText(this, wxID_ANY, wxString(time));
-    whiteTimerText->Move(1000*scal, 100*scal);
-    blackTimerText->Move(1000*scal, 500*scal);
+    whiteTimerText = new wxStaticText(rightPanel, wxID_ANY, wxString(time));
+    blackTimerText = new wxStaticText(rightPanel, wxID_ANY, wxString(time));
+    int whiteY = isWhite ? 500 : 100;
+    int blackY = isWhite ? 100 : 500;
+    whiteTimerText->Move(100*scal, whiteY*scal);
+    blackTimerText->Move(100*scal, blackY*scal);
 
     whiteTimer = new wxTimer(this, wxID_ANY);
     blackTimer = new wxTimer(this, wxID_ANY);
+
+    mainSizer->Add(leftPanel, wxSizerFlags(1).Expand());
+    mainSizer->Add(rightPanel, wxSizerFlags(1).Expand());
+
+    SetSizerAndFit(mainSizer);
+    Layout();
     
-    transparentPanel = new wxPanel(this, wxID_ANY, wxPoint(0,0), wxSize(1440*scal,810*scal));
+    transparentPanel = new wxPanel(this, wxID_ANY, wxPoint(0,0), this->GetSize());
     transparentPanel->SetBackgroundColour(wxColour(128, 120,120,120));
     loadingText = new wxTextCtrl(transparentPanel, wxID_ANY, wxString("Loading..."), wxDefaultPosition, transparentPanel->GetSize(), wxTE_CENTER | wxBORDER_NONE | wxTE_READONLY);  
     loadingText->SetForegroundColour(wxColour(0, 0, 0));
@@ -77,7 +89,6 @@ GameplayFrame::GameplayFrame(bool isWhite, GameOptions* options): wxFrame(NULL, 
     vSizer->AddStretchSpacer();
     transparentPanel->SetSizer(vSizer);
     transparentPanel->Show();
-    
 }
 
 bool checkTimerRunning(wxTimer* timer) {
@@ -151,7 +162,7 @@ int GameplayFrame::UpdateTime(wxTimerEvent& event) {
     // std::cout << "Id del timer di turno: " << timerId << " Id whiteTimer: " << whiteTimer->GetId() << " Id blackTimer: " << blackTimer->GetId() << std::endl;
     if (timerId == whiteTimer->GetId()) {
         whiteSeconds--;
-        whiteTimerText->SetLabel(secondsToString(whiteSeconds));
+            whiteTimerText->SetLabel(secondsToString(whiteSeconds));
         if (whiteSeconds == 0) {
             whiteTimer->Stop();
             return 1;
--- client/src/view/SettingsPanel.cpp
+++ client/src/view/SettingsPanel.cpp
@@ -4,53 +4,104 @@
 SettingsPanel::SettingsPanel(wxWindow* parent, SettingsConfiguration* configuration): wxPanel(parent) {
     SetSize(parent->GetSize());
     SetBackgroundColour(wxColour(118,150,86));
+
+    wxSizer *mainSizer = new wxBoxSizer(wxHORIZONTAL);
+
+    wxPanel *leftPanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+    wxPanel *rightPanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+    wxPanel *border = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+    border->SetBackgroundColour(wxColour(0,0,0));
+    border->SetSize(10, 810);
+
+    wxSizer *leftSizer = new wxBoxSizer(wxVERTICAL);
+    wxSizer *rightSizer = new wxBoxSizer(wxVERTICAL);
+    // leftSizer->SetBackgroundColour(wxColour(100,100,200));
     
     chessboard1 = img::GetImageAndScale(IMGPATH + "chessboard-brown.png", 0.1);
-    chessboard1Bitmap = new wxStaticBitmap(this, wxID_ANY, wxBitmap(chessboard1));
-    chessboard1Bitmap->Move(100, 10);
+    chessboard1Bitmap = new wxStaticBitmap(leftPanel, wxID_ANY, wxBitmap(chessboard1));
+    // chessboard1Bitmap->Move(100, 10);
 
     chessboard2 = img::GetImageAndScale(IMGPATH + "chessboard-blue.png", 0.1);
-    chessboard2Bitmap = new wxStaticBitmap(this, wxID_ANY, wxBitmap(chessboard2));
-    chessboard2Bitmap->Move(200, 10);
+    chessboard2Bitmap = new wxStaticBitmap(leftPanel, wxID_ANY, wxBitmap(chessboard2));
+    // chessboard2Bitmap->Move(200, 10);
 
     chessboard3 = img::GetImageAndScale(IMGPATH + "chessboard-black.png", 0.1);
-    chessboard3Bitmap = new wxStaticBitmap(this, wxID_ANY, wxBitmap(chessboard3));
-    chessboard3Bitmap->Move(300, 10);
+    chessboard3Bitmap = new wxStaticBitmap(leftPanel, wxID_ANY, wxBitmap(chessboard3));
+    // chessboard3Bitmap->Move(300, 10);
 
     chessboard1Bitmap->Bind(wxEVT_LEFT_DOWN, &SettingsPanel::Chessboard1Click, this);
     chessboard2Bitmap->Bind(wxEVT_LEFT_DOWN, &SettingsPanel::Chessboard2Click, this);
     chessboard3Bitmap->Bind(wxEVT_LEFT_DOWN, &SettingsPanel::Chessboard3Click, this);
 
     pieces1 = img::GetImageAndScale(IMGPATH + "icons/blackKnight.png", 1.44);
-    pieces1Bitmap = new wxStaticBitmap(this, wxID_ANY, wxBitmap(pieces1));
-    pieces1Bitmap->Move(100, 150);
+    pieces1Bitmap = new wxStaticBitmap(leftPanel, wxID_ANY, wxBitmap(pieces1));
+    // pieces1Bitmap->Move(100, 150);
 
     pieces2 = img::GetImageAndScale(IMGPATH + "icons/whiteKnight.png", 1.44);
-    pieces2Bitmap = new wxStaticBitmap(this, wxID_ANY, wxBitmap(pieces2));
-    pieces2Bitmap->Move(200, 150);
+    pieces2Bitmap = new wxStaticBitmap(leftPanel, wxID_ANY, wxBitmap(pieces2));
+    // pieces2Bitmap->Move(200, 150);
 
     pieces3 = img::GetImageAndScale(IMGPATH + "icons2/black_horse.png", 1.8);
-    pieces3Bitmap = new wxStaticBitmap(this, wxID_ANY, wxBitmap(pieces3));
-    pieces3Bitmap->Move(300, 150);
+    pieces3Bitmap = new wxStaticBitmap(leftPanel, wxID_ANY, wxBitmap(pieces3));
+    // pieces3Bitmap->Move(300, 150);
 
     pieces1Bitmap->Bind(wxEVT_LEFT_DOWN, &SettingsPanel::Pieces1Click, this);
     pieces2Bitmap->Bind(wxEVT_LEFT_DOWN, &SettingsPanel::Pieces2Click, this);
     pieces3Bitmap->Bind(wxEVT_LEFT_DOWN, &SettingsPanel::Pieces3Click, this);
 
-    btnBack = new wxButton(this, wxID_ANY, "Back",wxPoint(100,500));
-    btnSave = new wxButton(this, wxID_ANY, "Save",wxPoint(200,500));
+    btnBack = new wxButton(leftPanel, wxID_ANY, "Back",wxDefaultPosition,wxDefaultSize);
+    btnSave = new wxButton(leftPanel, wxID_ANY, "Save",wxDefaultPosition,wxDefaultSize);
 
-    wxStaticText* staticText = new wxStaticText(this, wxID_ANY, "Configurazione attuale (quella sotto)", wxDefaultPosition, wxDefaultSize, wxALIGN_CENTER);
-    staticText->SetPosition(wxPoint(150, 250)); // Imposta la posizione
+    wxStaticText* staticText = new wxStaticText(rightPanel, wxID_ANY, "Configurazione attuale (quella sotto)", wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
+    // staticText->SetPosition(wxPoint(150, 250)); // Imposta la posizione
     // selectedChessboard = chessboard3;
-    chessboardSelectedBitmap = new wxStaticBitmap(this, wxID_ANY, wxNullBitmap);
-    chessboardSelectedBitmap->Move(300, 300);
+    chessboardSelectedBitmap = new wxStaticBitmap(rightPanel, wxID_ANY, wxNullBitmap);
+    // chessboardSelectedBitmap->Move(300, 300);
     // selectedPieces = pieces1;
-    selectedPiecesBitmap = new wxStaticBitmap(this, wxID_ANY, wxNullBitmap);
-    selectedPiecesBitmap->Move(150, 300);
+    selectedPiecesBitmap = new wxStaticBitmap(rightPanel, wxID_ANY, wxNullBitmap);
+    // selectedPiecesBitmap->Move(150, 300);
     //-----------------------
     SetSettingsConfig(configuration);
 
+
+    wxStaticText* chooseText = new wxStaticText(this, wxID_ANY, "Scegliere una configurazione", wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
+    leftSizer->Add(chooseText, wxSizerFlags(0).Expand().Border(wxALL, 5));
+
+    wxStaticText* chessboardText = new wxStaticText(this, wxID_ANY, "Tipo di scacchiera", wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
+    leftSizer->Add(chessboardText, wxSizerFlags(0).Expand().Border(wxALL, 5));
+
+    wxSizer *chessboardSizer = new wxBoxSizer(wxHORIZONTAL);
+    chessboardSizer->Add(chessboard1Bitmap, wxSizerFlags(1).Align(wxALIGN_CENTER).Border(wxALL, 5));
+    chessboardSizer->Add(chessboard2Bitmap, wxSizerFlags(1).Align(wxALIGN_CENTER).Border(wxALL, 5));
+    chessboardSizer->Add(chessboard3Bitmap, wxSizerFlags(1).Align(wxALIGN_CENTER).Border(wxALL, 5));
+    leftSizer->Add(chessboardSizer, wxSizerFlags(0).Expand().Border(wxALL, 5));
+
+    wxStaticText* piecesText = new wxStaticText(this, wxID_ANY, "Tipo di pezzi", wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
+    leftSizer->Add(piecesText, wxSizerFlags(0).Expand().Border(wxALL, 5));
+
+    wxSizer *piecesSizer = new wxBoxSizer(wxHORIZONTAL);
+    piecesSizer->Add(pieces1Bitmap, wxSizerFlags(1).Align(wxALIGN_CENTER).Border(wxALL, 5));
+    piecesSizer->Add(pieces2Bitmap, wxSizerFlags(1).Align(wxALIGN_CENTER).Border(wxALL, 5));
+    piecesSizer->Add(pieces3Bitmap, wxSizerFlags(1).Align(wxALIGN_CENTER).Border(wxALL, 5));
+    leftSizer->Add(piecesSizer, wxSizerFlags(0).Expand().Border(wxALL, 5));
+
+    wxSizer *btnSizer = new wxBoxSizer(wxHORIZONTAL);
+    btnSizer->Add(btnBack, wxSizerFlags(1).Align(wxALIGN_CENTER).Border(wxALL, 5));
+    btnSizer->Add(btnSave, wxSizerFlags(1).Align(wxALIGN_CENTER).Border(wxALL, 5));
+    leftSizer->Add(btnSizer, wxSizerFlags(4).Expand().Border(wxALL, 5));
+
+    rightSizer->Add(staticText, wxSizerFlags(0).Expand().Border(wxALL, 5));
+    rightSizer->Add(chessboardSelectedBitmap, wxSizerFlags(0).Expand().Border(wxALL, 5));
+    rightSizer->Add(selectedPiecesBitmap, wxSizerFlags(0).Expand().Border(wxALL, 5));
+
+    leftPanel->SetSizer(leftSizer);
+    rightPanel->SetSizer(rightSizer);
+
+    mainSizer->Add(leftPanel, wxSizerFlags(1).Expand());
+    mainSizer->Add(border, wxSizerFlags(0).Expand());
+    mainSizer->Add(rightPanel, wxSizerFlags(1).Expand());
+    SetSizer(mainSizer);
+    Layout();
     // TODO: Scacchiera di riferimento per vedere la configurazione con tutti i pezzi della scacchiera
 }
 
@@ -93,11 +144,13 @@ void SettingsPanel::Chessboard3Click(wxMouseEvent&) {
 void SettingsPanel::setSelectedChessboardBitmap(wxImage image, wxStaticBitmap* bitmap){
     selectedChessboard = image;
     chessboardSelectedBitmap->SetBitmap(bitmap->GetBitmap());
+    Layout();
 }
 
 void SettingsPanel::setSelectedPiecesBitmap(wxImage image, wxStaticBitmap* bitmap){
     selectedPieces = image;
     selectedPiecesBitmap->SetBitmap(bitmap->GetBitmap());
+    Layout();
 }
 
 void SettingsPanel::Pieces1Click(wxMouseEvent& event) {
