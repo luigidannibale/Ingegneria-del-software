22d3d7a2b3d892aa31aaa7a7eb0d8b19b623aab8
luigidannibale <dannibale.1991254@studenti.uniroma1.it>
2024-06-18 18:55:39 +0200

    Entire restyle of code
    No more using makefile, now using cmake, furthermore dividing project onto 2 subprojects: client and server

--- client/src/lib.hpp
+++ client/src/lib.hpp
@@ -1,6 +1,7 @@
 #ifndef INGEGERIA_DEGLI_SCACCHI_LIB_H
 #define INGEGERIA_DEGLI_SCACCHI_LIB_H
 
-#include <wx-3.0/wx/wx.h>
+//#include <wx-3.0/wx/wx.h>
+#include <wx-3.2/wx/wx.h>
 
 #endif //INGEGERIA_DEGLI_SCACCHI_LIB_H
